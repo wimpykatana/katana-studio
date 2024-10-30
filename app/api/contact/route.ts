aa1e420e111dff372595f48bd5bd2fa69a6e3eb6
Wimpy Katana <wim.katana@gmail.com>
2024-10-30 11:50:53 +0700

    fix roter issue

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,9 +2,7 @@
 import { transporter, mailOptions } from "@/config/nodemailer";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  //export default async function handler(  req: NextApiRequest,res: NextApiResponse,) {
-  //if (req.method === "POST") {
+export async function POST(req: NextRequest) {
   const data = await req.json();
 
   try {
@@ -14,14 +12,21 @@ export async function POST(req: NextRequest, res: NextResponse) {
       text: "Email from katana studio website",
       html: `<h1>${data.subject}</h1><p>name: ${data.name}</p><p>email: ${data.email}</p><p>phone: ${data.phone}</p></hr><p>message:</p><p>${data.message}</p>`,
     });
-    return new Response(JSON.stringify({ message: "Email sent" }), {
-      headers: { "Content-Type": "application/json" },
-    });
+    //return new Response(JSON.stringify({ message: "Email sent" }), {
+    //  headers: { "Content-Type": "application/json" },
+    //});
+    return NextResponse.json(
+      { message: "Form submitted successfully" },
+      { status: 200 },
+    );
   } catch (error) {
     console.error(error);
-    return new Response(JSON.stringify({ message: error.message }), {
-      headers: { "Content-Type": "application/json" },
-    });
+    //return new Response(JSON.stringify({ message: error.message }), {
+    //  headers: { "Content-Type": "application/json" },
+    //});
+    return NextResponse.json(
+      { message: "Failed to process form" },
+      { status: 500 },
+    );
   }
-  //}
 }
